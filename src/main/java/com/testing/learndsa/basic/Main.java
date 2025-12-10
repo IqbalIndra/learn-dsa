e6ef6641cd4c3cc9ec5198f3bcc0e25635d61f46
iqbal.indra <iqbal.indra@karmaclub.id>
2025-12-10 08:58:37 +0700

    Add method for append node

---
+++ src/main/java/com/testing/learndsa/basic/Main.java
@@ -0,0 +1,10 @@
+package com.testing.learndsa.basic;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(1);
+        linkedList.append(2);
+        linkedList.append(3);
+        linkedList.printList();
+    }
+}
