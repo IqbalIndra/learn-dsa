3f4ce6d6af06260e45eae8f984877ee015f9d8f2
iqbal.indra <iqbal.indra@karmaclub.id>
2025-12-16 21:11:23 +0700

    solution for sub array sum with k for total num

--- src/main/java/com/testing/learndsa/hashmap/Main.java
+++ src/main/java/com/testing/learndsa/hashmap/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] res = solution.subarraySum(new int[]{1, 2, 3, 4, 5}, 9);
-        System.out.println(Arrays.toString(res));
+        int res = solution.subarraySum(new int[]{2,-1,1,2}, 2);
+        System.out.println(res);
     }
 }
--- src/main/java/com/testing/learndsa/hashmap/Solution.java
+++ src/main/java/com/testing/learndsa/hashmap/Solution.java
@@ -5,49 +5,18 @@ import java.util.Map;
 
 public class Solution {
 
-    public int[] subarraySum(int[] nums, int target){
-        //1, 2, 3, 4, 5
+    public int subarraySum(int[] nums, int target){
+        //2,-1,1,2
 
-        // prepopulate to hashmap {0:-1}
-
-        // currSum= 1
-        // currSum - target = 1-9 = -8
-        // check {0:-1} = -8
-        // {0:-1 , 1 : 0}
-
-        // {0:-1 , 1 : 0}
-        // currSum = 3
-        // currSum - target = 3-9 = -6
-        // check {0:-1 , 1 : 0} = -6
-        // {0:-1 , 1 : 0 , 3 : 1}
-
-        // {0:-1 , 1 : 0 , 3 : 1}
-        // currSum = 6
-        // currSum - target = 6-9 = -3
-        // check {0:-1 , 1 : 0, 3 : 1} = -3
-        // {0:-1 , 1 : 0 , 3 : 1, 6 : 2}
-
-        // {0:-1 , 1 : 0 , 3 : 1, 6 : 2}
-        // currSum = 10
-        // currSum - target = 10-9 = 1
-        // check {0:-1 , 1 : 0 , 3 : 1, 6 : 2} = 1
-        //check (currSum - target) =0 + 1 (find in hashmap key)
-        //res = res[0] = 1 , res[1] = index
-        
-        int currSum = 0;
-        int index = 0;
+        int currSum = 0, res = 0;
         Map<Integer,Integer> map = new HashMap<>();
-        map.put(0,-1);
-        int[] res = new int[2];
+        map.put(0,1);
         for(int num : nums){
             currSum+=num;
             int compliment = currSum - target;
-            if(map.containsKey(compliment)){
-                res[0] = map.get(compliment) +1;
-                res[1] = index;
-                break;
-            }
-            map.putIfAbsent(currSum, index++);
+            res+=map.getOrDefault(compliment, 0);
+
+            map.put(currSum, map.getOrDefault(currSum,0)+1);
         }
 
         return res;
